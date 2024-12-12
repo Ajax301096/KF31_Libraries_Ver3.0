e6eb10d6ea22666083bf32e7f5b3d25f8dafcffe
ajax301096 <147789857+ajax301096@users.noreply.github.com>
2024-12-12 13:34:23 +0900

    #

--- KF31_図書館システム版3.0/ViewModel/LoanViewModel.cs
+++ KF31_図書館システム版3.0/ViewModel/LoanViewModel.cs
@@ -77,6 +77,7 @@ namespace KF31_図書館システム版3._0.ViewModel
         public ICommand EnableDatePickerCommand { get; set; }
         public ICommand SearchLoanCommand { get; set; }
         public ICommand StatusUpdateCommand { get; set; }
+        public ICommand BarCodeCommand { get; set; }
 
         public LoanViewModel()
         {
@@ -303,6 +304,19 @@ namespace KF31_図書館システム版3._0.ViewModel
 
 
                 });
+            BarCodeCommand = new RelayCommand<Window>(
+                (p) =>
+                {
+                    return true;
+                },
+                (p) =>
+                {
+                    StartBarcodeScanning();
+                });
+        }
+        private void StartBarcodeScanning()
+        {
+
         }
         private void EnableDatePicker()
         {
